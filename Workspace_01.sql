0c8441d356378d6c313304579dc94e8c9dd37740
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-13 01:44:02 +0000

    Tue, Feb 13, 2024, 1:44 AM +00:00

--- Workspace_01.sql
+++ Workspace_01.sql
@@ -1,11 +1,11 @@
 -- Question 1 List all products whose unit price is less than the average 
 -- price. 1 mark
-SELECT PRODUCT_ID, PRODUCT_NAME, UNITPRICE
+SELECT ProductID, ProductName, UnitPrice
 FROM
-    PRODUCTS
+    Products
 WHERE
-    UNITPRICE < (
-        SELECT AVG(UNITPRICE)
+    UnitPrice < (
+        SELECT AVG(UnitPrice)
         FROM
             PRODUCTS
     );
